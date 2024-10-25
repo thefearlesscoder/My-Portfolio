adf907c470a667e9f59e11918f6352dd22cd5a6b
thefearlesscoder <thefunnelvkd@gmail.com>
2024-10-25 11:55:17 +0530

    create skill component

--- dashboard/src/pages/subComponents/AddSkill.jsx
+++ dashboard/src/pages/subComponents/AddSkill.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import {
+  addNewSkill,
+  clearAllSkillErrors,
+  getAllSkills,
+  resetSkillSlice,
+} from "@/store/slices/skillSlice";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { Button } from "@/components/ui/button";
+import SpecialLoadingButton from "./SpecialLoadingButton";
 
 const AddSkill = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [proficiency, setProficiency] = useState("");
+  const [svg, setSvg] = useState("");
+  const [svgPreview, setSvgPreview] = useState("");
 
-export default AddSkill
+  const handleSvg = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      setSvgPreview(reader.result);
+      setSvg(file);
+    };
+  };
+
+  const { loading, error, message } = useSelector((state) => state.skill);
+  const dispatch = useDispatch();
+  const handleAddNewSkill = (skill) => {
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("proficiency", proficiency);
+    formData.append("svg", svg);
+    dispatch(addNewSkill(formData));
+  };
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch(clearAllSkillErrors());
+    }
+    if (message) {
+      toast.success(message);
+      dispatch(resetSkillSlice());
+      dispatch(getAllSkills());
+    }
+  }, [dispatch, loading, error]);
+
+  return <div></div>;
+};
+
+export default AddSkill;
